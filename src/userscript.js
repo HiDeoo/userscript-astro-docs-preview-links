ca787e57a872b29754c505bbece44af32bace62b
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-09-27 16:02:49 +0200

    feat: basic links

--- src/userscript.js
+++ src/userscript.js
@@ -6,25 +6,96 @@
   const comments = [...document.querySelectorAll('.pull-discussion-timeline .timeline-comment')]
 
   /**
+   * @param {string} author
+   * @returns {Element[]}
+   */
+  function getCommentsFromAuthor(author) {
+    return comments.filter((comment) =>
+      isElementTextEqual(comment.querySelector('.timeline-comment-header .author'), author),
+    )
+  }
+
+  /**
    * @param {Element | null} element
    * @param {string} text
+   * @returns {boolean}
    */
   function isElementTextEqual(element, text) {
     return element instanceof HTMLElement && element.innerText === text
   }
 
-  const houstonBotComments = comments.filter((comment) =>
-    isElementTextEqual(comment.querySelector('.timeline-comment-header .author'), 'astrobot-houston'),
-  )
+  /**
+   * @param {string} path
+   * @returns {string}
+   */
+  function stripExtension(path) {
+    const periodIndex = path.lastIndexOf('.')
+    return path.slice(0, periodIndex > -1 ? periodIndex : undefined)
+  }
 
-  if (houstonBotComments.length === 0) return
+  /**
+   * @param {string} locale
+   * @returns {boolean}
+   */
+  function isRootLocale(locale) {
+    return window.location.href.startsWith('https://github.com/withastro/starlight/pull/') && locale === 'en'
+  }
 
-  const lunariaComment = houstonBotComments.find((comment) =>
+  const deployComment = getCommentsFromAuthor('netlify').find((comment) => {
+    const title = comment.querySelector('.comment-body > h3:first-child')
+    return title instanceof HTMLElement && title.innerText.includes('Deploy Preview for')
+  })
+  if (!deployComment) return
+
+  const deployPreviewRow = [...deployComment.querySelectorAll('.comment-body td')].find((cell) =>
+    isElementTextEqual(cell, '😎 Deploy Preview'),
+  )?.parentElement
+  if (!deployPreviewRow) return
+
+  const deployPreviewUrl = deployPreviewRow.querySelector('a')?.href
+  if (!deployPreviewUrl) return
+
+  const lunariaComment = getCommentsFromAuthor('astrobot-houston').find((comment) =>
     isElementTextEqual(comment.querySelector('.comment-body > h2:first-child'), 'Lunaria Status Overview'),
   )
-
   if (!lunariaComment) return
 
-  // FIXME(HiDeoo)
-  console.error('🚨 [userscript.js:28] lunariaComment:', lunariaComment)
+  const trackedFileTable = [...lunariaComment.querySelectorAll('.comment-body > h3')].find((heading) =>
+    isElementTextEqual(heading, 'Tracked Files'),
+  )?.nextElementSibling
+  if (!trackedFileTable) return
+  const trackedFilesRows = [...trackedFileTable.querySelectorAll('table > tbody > tr')]
+
+  /** @type {{ locale: string, path: string }[]} */
+  const trackedFiles = []
+
+  for (const row of trackedFilesRows) {
+    const [locale, path] = [...row.querySelectorAll('td')].map((cell) => cell.innerText)
+    if (!locale || !path) continue
+    trackedFiles.push({ locale: locale, path })
+  }
+
+  if (trackedFiles.length === 0) return
+
+  const linksRow = document.createElement('tr')
+
+  const linksTitleCell = document.createElement('td')
+  linksTitleCell.setAttribute('align', 'center')
+  linksTitleCell.innerText = '⚡ Tracked links'
+
+  const linksContentCell = document.createElement('td')
+  linksContentCell.append(
+    ...trackedFiles.flatMap(({ locale, path }, index) => {
+      const pathname = `${isRootLocale(locale) ? '' : `${locale}/`}${stripExtension(path)}`
+
+      const link = document.createElement('a')
+      link.href = deployPreviewUrl + pathname
+      link.innerText = pathname
+
+      return index < trackedFiles.length - 1 ? [link, document.createElement('br')] : link
+    }),
+  )
+
+  linksRow.append(linksTitleCell, linksContentCell)
+  deployPreviewRow.after(linksRow)
 })()
